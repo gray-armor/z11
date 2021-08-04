9e9b009fa8b7aeff5e87fb18d2cb2e55c290ee16
shierote <taishi2060@gmail.com>
2021-08-05 03:35:57 +0900

    Extend drawing primitives

--- libz11/render_block.c
+++ libz11/render_block.c
@@ -159,10 +159,26 @@ static GLenum z_render_block_get_current_state_opengl_topology_mode(struct z_ren
   enum z11_gl_topology topology = z_render_block_state_get_topology(render_block->current_state);
 
   switch (topology) {
+    case Z11_GL_TOPOLOGY_POINTS:
+      return GL_POINTS;
     case Z11_GL_TOPOLOGY_LINES:
       return GL_LINES;
+    case Z11_GL_TOPOLOGY_LINE_STRIP:
+      return GL_LINE_STRIP;
+    case Z11_GL_TOPOLOGY_LINE_LOOP:
+      return GL_LINE_LOOP;
     case Z11_GL_TOPOLOGY_TRIANGLES:
       return GL_TRIANGLES;
+    case Z11_GL_TOPOLOGY_TRIANGLE_STRIP:
+      return GL_TRIANGLE_STRIP;
+    case Z11_GL_TOPOLOGY_TRIANGLE_FAN:
+      return GL_TRIANGLE_FAN;
+    case Z11_GL_TOPOLOGY_QUADS:
+      return GL_QUADS;
+    case Z11_GL_TOPOLOGY_QUAD_STRIP:
+      return GL_QUAD_STRIP;
+    case Z11_GL_TOPOLOGY_POLYGON:
+      return GL_POLYGON;
     default:
       return GL_LINES;
   }
