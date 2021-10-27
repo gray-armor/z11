f812bb513e8fc6cec0308a4dd3ba2c501fe996a4
Aki-7 <aki.develop8128@gmail.com>
2021-10-27 23:45:41 +0900

    apply https://github.com/gray-armor/z11/pull/73#discussion_r736707236

--- libzazen/compositor.h
+++ libzazen/compositor.h
@@ -18,7 +18,7 @@ struct zazen_compositor {
 struct zazen_compositor* zazen_compositor_create(struct wl_display* display);
 
 // return null when no cuboid window intersected
-// local_ray_* and min_distance will be set only when intersected
+// local_ray_* and distance will be set only when intersected
 struct zazen_cuboid_window* zazen_compositor_pick_cuboid_window(
     struct zazen_compositor* compositor, vec3 ray_origin, vec3 ray_direction,
     vec3 local_ray_origin, vec3 local_ray_direction, float* distance);
